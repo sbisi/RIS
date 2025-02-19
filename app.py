513a44aa118bc3991322a2af69f049415d53837f
sbisi <62792590+sbisi@users.noreply.github.com>
2025-02-19 18:19:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -245,4 +245,4 @@ def toggle_sidebar(n_clicks):
 
 if __name__ == "__main__":
     app.config.suppress_callback_exceptions = True
-    app.run_server(host="0.0.0.0", port=10000, debug=True)
+    app.run_server(host="0.0.0.0", port=10000, debug=False)
